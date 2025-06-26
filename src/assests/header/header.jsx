64ac817369d8b9d5499d10a056fe58ada5712bbc
Noskash <noskashik@gmail.com>
2025-06-26 15:15:39 +0500

    первый коммит

--- src/assests/header/header.jsx
+++ src/assests/header/header.jsx
@@ -1,20 +1,27 @@
 import tonlogo from '../currency.png';
 import './header.css';
 import Button from '../button/button';
-export default function Header(props){
+
+export default function Header(props) {
     return (
         <header className='header'>
-            <img src = {props.logo} className='logo' alt="logo"/>
-            {
-                props.isRegistered ? (
-                    <span className = "balance">{props.balance} <img className="ton-icon"src={tonlogo} alt="ton"/></span>
-                
-                ) : (
-                    <div>
-                        <Button text="Зарегистрироватся" />
-                    </div>
-                )
-            }
+            <img 
+                src={props.logo} 
+                className='logo' 
+                alt="logo"
+                onError={(e) => {
+                    e.target.src = 'https://via.placeholder.com/50';
+                }}
+            />
+            {props.isRegistered ? (
+                <span className="balance">
+                    {props.balance} <img className="ton-icon" src={tonlogo} alt="ton"/>
+                </span>
+            ) : (
+                <div>
+                    <Button text="Зарегистрироватся" />
+                </div>
+            )}
         </header>
     );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const script = document.createElement('script');
+script.src = 'https://telegram.org/js/telegram-web-app.js';
+script.async = true;
+document.head.appendChild(script);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +16,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
